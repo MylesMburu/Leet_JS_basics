14164e3fa4d64f70aa7948d334ffe310669ad281
MylesMburu <ngicumyles@gmail.com>
2023-07-06 23:15:53 +0300

    .

--- Closures/Counter2.js/Counter2.js
+++ Closures/Counter2.js/Counter2.js
@@ -1,3 +1,11 @@
+// Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+
+// The three functions are:
+
+// increment() increases the current value by 1 and then returns it.
+// decrement() reduces the current value by 1 and then returns it.
+// reset() sets the current value to init and then returns it.
+
 var createCounter = function(init) {
     let n = init;
     return {
